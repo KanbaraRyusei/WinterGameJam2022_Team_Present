048c2bcbef05ddd11c03dc1b1bdae04ec8effc59
DRGO <abokado0901@gmail.com>
2022-12-22 13:16:11 +0900

    カウントダウン追加

--- Assets/Iwadare/GameOverLineScript.cs
+++ Assets/Iwadare/GameOverLineScript.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 
 public class GameOverLineScript : MonoBehaviour
 {
+    [SerializeField]string _sceneName;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.gameObject.tag == "Player")
         {
             Debug.Log("ƒ„ƒ‰ƒŒƒ`ƒƒƒbƒ^");
+            SceneLoder.LoadScene(_sceneName);
         }
     }
 }
